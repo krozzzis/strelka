dbe21a73f5cf46b7e3b9f4a9a3f0e67861bfba67
krozzzis <schumov.nn@gmail.com>
2024-06-23 01:23:07 +0500

    Some optimization and caching

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use iced::mouse::{self, Cursor, Interaction};
-use iced::widget::canvas::{self, event::Status, Canvas, Cache, Event, Frame, Geometry, Program};
 use iced::widget::canvas::path;
+use iced::widget::canvas::{self, event::Status, Cache, Canvas, Event, Frame, Geometry, Program};
 use iced::Color;
 use iced::Length;
 use iced::Point;
 use iced::Rectangle;
 use iced::Renderer;
 use iced::Theme;
-use iced::Vector;
 use iced::{Element, Sandbox, Settings};
 use rand::prelude::*;
 
@@ -20,6 +19,7 @@ pub struct Stroke {
 #[derive(Default)]
 struct CanvasState {
     cache: Cache,
+    stroke: Stroke,
 }
 
 #[derive(Default)]
@@ -29,13 +29,17 @@ pub struct App {
 }
 
 #[derive(Debug, Clone)]
-pub enum Mess {
+pub enum AppMessage {
     AddStroke(Stroke),
+    AddPointToStroke(Point),
+    ChangeStrokeColor(Color),
+    EndStroke,
+    RemoveLastStroke,
     ClearStrokes,
 }
 
 impl Sandbox for App {
-    type Message = Mess;
+    type Message = AppMessage;
 
     fn new() -> Self {
         App::default()
@@ -49,24 +53,44 @@ impl Sandbox for App {
         match message {
             Self::Message::AddStroke(x) => {
                 self.strokes.push(x);
+                self.canvas_state.cache.clear();
+            }
+
+            Self::Message::AddPointToStroke(x) => {
+                self.canvas_state.stroke.points.push(x);
+                self.canvas_state.cache.clear();
+            }
+
+            Self::Message::EndStroke => {
+                self.strokes.push(self.canvas_state.stroke.clone());
+                self.canvas_state.stroke.points.clear();
+                self.canvas_state.cache.clear();
+            }
+
+            Self::Message::ChangeStrokeColor(x) => {
+                self.canvas_state.stroke.color = x;
             }
 
             Self::Message::ClearStrokes => {
                 self.strokes.clear();
+                self.canvas_state.cache.clear();
+            }
+
+            Self::Message::RemoveLastStroke => {
+                self.strokes.pop();
+                self.canvas_state.cache.clear();
             }
         }
     }
 
     fn view(&self) -> Element<Self::Message> {
-        let canvas: Element<Stroke> = Canvas::new(Field {
+        Canvas::new(Field {
             strokes: &self.strokes,
             canvas_state: &self.canvas_state,
         })
         .width(Length::Fill)
         .height(Length::Fill)
-        .into();
-
-        canvas.map(Self::Message::AddStroke)
+        .into()
     }
 }
 
@@ -80,8 +104,6 @@ pub enum FieldMouse {
 #[derive(Default, Debug, Clone)]
 pub struct FieldState {
     mouse: FieldMouse,
-    points: Vec<Point>,
-    color: Color,
 }
 
 struct Field<'a> {
@@ -90,7 +112,7 @@ struct Field<'a> {
 }
 
 // Then, we implement the `Program` trait
-impl<'a> Program<Stroke> for Field<'a> {
+impl<'a> Program<AppMessage> for Field<'a> {
     type State = FieldState;
 
     fn draw(
@@ -101,41 +123,51 @@ impl<'a> Program<Stroke> for Field<'a> {
         bounds: Rectangle,
         _cursor: mouse::Cursor,
     ) -> Vec<Geometry> {
-        // We prepare a new `Frame`
-        let mut frame = Frame::new(renderer, bounds.size());
-
-        for i in 0..self.strokes.len() {
-            let stroke = &self.strokes[i];
-            if stroke.points.len() > 0 {
+        let draw_stroke = |stroke: &Stroke, frame: &mut Frame| {
+            if !stroke.points.is_empty() {
                 let mut builder = path::Builder::new();
 
                 builder.move_to(stroke.points[0]);
                 for pos in &stroke.points {
                     builder.line_to(*pos);
                 }
-                frame.stroke(&builder.build(), canvas::Stroke {
-                    width: 2.0,
-                    style: canvas::Style::Solid(stroke.color),
-                    line_cap: canvas::LineCap::Round,
-                    line_join: canvas::LineJoin::Round,
-                    line_dash: canvas::LineDash::default(),
-                });
+
+                frame.stroke(
+                    &builder.build(),
+                    canvas::Stroke {
+                        width: 5.0,
+                        style: canvas::Style::Solid(stroke.color),
+                        line_cap: canvas::LineCap::Round,
+                        line_join: canvas::LineJoin::Round,
+                        line_dash: canvas::LineDash::default(),
+                    },
+                );
             }
-        }
+        };
 
-        //for v in self.poses {
-        //    // We create a `Path` representing a simple circle
-        //    let circle = Path::circle(Point::new(v.x, v.y), circle_radius);
-        //
-        //    // And fill it with some color
-        //    frame.fill(&circle, Color::BLACK);
-        //}
+        let content = self
+            .canvas_state
+            .cache
+            .draw(renderer, bounds.size(), |frame: &mut Frame| {
+                for i in 0..self.strokes.len() {
+                    let stroke = &self.strokes[i];
+                    draw_stroke(stroke, frame);
+                }
+            });
 
-        // Finally, we produce the geometry
-        vec![frame.into_geometry()]
+        let mut frame = Frame::new(renderer, bounds.size());
+
+        draw_stroke(&self.canvas_state.stroke, &mut frame);
+
+        vec![content, frame.into_geometry()]
     }
 
-    fn mouse_interaction(&self, _state: &Self::State, bounds: Rectangle, cursor: Cursor) -> Interaction {
+    fn mouse_interaction(
+        &self,
+        _state: &Self::State,
+        bounds: Rectangle,
+        cursor: Cursor,
+    ) -> Interaction {
         if cursor.is_over(bounds) {
             mouse::Interaction::Crosshair
         } else {
@@ -149,58 +181,49 @@ impl<'a> Program<Stroke> for Field<'a> {
         event: Event,
         bounds: Rectangle,
         cursor: Cursor,
-    ) -> (Status, Option<Stroke>) {
+    ) -> (Status, Option<AppMessage>) {
         if let Some(cursor_pos) = cursor.position_in(bounds) {
             match event {
                 Event::Mouse(mouse_event) => match mouse_event {
-                    mouse::Event::ButtonPressed(mouse::Button::Left) => {
-                        match state.mouse {
-                            FieldMouse::Idle => {
-                                state.mouse = FieldMouse::Stroking;
-                                let mut rng = rand::thread_rng();
-                                state.color = Color::new(
-                                    rng.gen_range(0.0..1.0), 
-                                    rng.gen_range(0.0..1.0), 
-                                    rng.gen_range(0.0..1.0), 
-                                    1.0
-                                    );
-                                state.points.push(cursor_pos);
-                            },
-                            FieldMouse::Stroking => {
-                                state.points.push(cursor_pos);
-                            },
+                    mouse::Event::ButtonPressed(mouse::Button::Right) => {
+                        (Status::Captured, Some(AppMessage::RemoveLastStroke))
+                    }
+                    mouse::Event::ButtonPressed(mouse::Button::Left) => match state.mouse {
+                        FieldMouse::Idle => {
+                            state.mouse = FieldMouse::Stroking;
+                            let mut rng = rand::thread_rng();
+                            let color = Color::new(
+                                rng.gen_range(0.0..1.0),
+                                rng.gen_range(0.0..1.0),
+                                rng.gen_range(0.0..1.0),
+                                1.0,
+                            );
+                            (Status::Captured, Some(AppMessage::ChangeStrokeColor(color)))
                         }
-                        (Status::Captured, None)
+                        FieldMouse::Stroking => (
+                            Status::Captured,
+                            Some(AppMessage::AddPointToStroke(cursor_pos)),
+                        ),
                     },
 
                     mouse::Event::ButtonReleased(mouse::Button::Left) => {
                         match state.mouse {
-                            FieldMouse::Idle => {
-                                state.points.push(cursor_pos);
-                            },
+                            FieldMouse::Idle => {}
                             FieldMouse::Stroking => {
                                 state.mouse = FieldMouse::Idle;
-                                state.points.push(cursor_pos);
-                            },
+                            }
                         }
-                        state.points.clear();
-                        (Status::Captured, Some(Stroke {
-                            points: state.points.clone(),
-                            color: state.color,
-                        }))
-                    },
+                        (Status::Captured, Some(AppMessage::EndStroke))
+                    }
 
-                    mouse::Event::CursorMoved { position: cursor_pos } => {
-                        match state.mouse {
-                            FieldMouse::Stroking => {
-                                state.points.push(cursor_pos);
-                            },
-                            _ => {},
-                        }
-                        (Status::Captured, Some(Stroke {
-                            points: state.points.clone(),
-                            color: state.color,
-                        }))
+                    mouse::Event::CursorMoved {
+                        position: cursor_pos,
+                    } => match state.mouse {
+                        FieldMouse::Stroking => (
+                            Status::Captured,
+                            Some(AppMessage::AddPointToStroke(cursor_pos)),
+                        ),
+                        FieldMouse::Idle => (Status::Captured, None),
                     },
 
                     _ => (Status::Ignored, None),
