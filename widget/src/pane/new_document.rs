dc40f0be323bd0daf299fa32dbcefefefb49880c
krozzzis <schumov.nn@gmail.com>
2024-08-19 13:11:05 +0500

    Updated NewDocument pane

--- widget/src/pane/new_document.rs
+++ widget/src/pane/new_document.rs
@@ -1,6 +1,6 @@
 use iced::{
-    widget::{center, column},
-    Element,
+    widget::{center, column, text},
+    Alignment, Element, Font, Length,
 };
 use theming::Theme;
 
@@ -12,8 +12,22 @@ pub enum Message {
 }
 
 pub fn new_document_pane<'a>() -> Element<'a, Message, Theme> {
-    background(center(column![
-        text_button("Open file Ctrl+O").on_press(Message::PickFile),
-    ]))
+    background(center(
+        column![
+            center(
+                text("No file is open")
+                    .font(Font {
+                        weight: iced::font::Weight::Bold,
+                        ..Default::default()
+                    })
+                    .align_x(Alignment::Center)
+                    .size(24.0)
+            )
+            .height(Length::Shrink),
+            center(text_button("Open file Ctrl+O").on_press(Message::PickFile))
+                .height(Length::Shrink),
+        ]
+        .spacing(16.0),
+    ))
     .into()
 }
