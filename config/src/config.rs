a082f9983a04decf5a5370fa5e519b29b3499e0e
krozzzis <schumov.nn@gmail.com>
2025-01-08 00:36:12 +0500

    Replace key in `Config` with new `Key` struct

--- config/src/config.rs
+++ config/src/config.rs
@@ -7,80 +7,87 @@ use std::path::PathBuf;
 use dashmap::mapref::one::Ref;
 use dashmap::DashMap;
 
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub struct Key {
+    pub namespace: SmolStr,
+    pub id: SmolStr,
+}
+
+#[derive(Debug, Clone)]
 pub struct Config {
-    data: DashMap<SmolStr, Value>,
+    data: DashMap<Key, Value>,
 }
 
 impl Config {
-    pub fn get_value(&self, key: &SmolStr) -> Option<Ref<SmolStr, Value>> {
+    pub fn get_value(&self, key: &Key) -> Option<Ref<Key, Value>> {
         self.data.get(key)
     }
 
-    pub fn set_value(&mut self, key: SmolStr, value: Value) {
+    pub fn set_value(&mut self, key: Key, value: Value) {
         self.data.insert(key, value);
     }
 
-    pub fn get_integer(&self, key: &SmolStr) -> Option<i64> {
+    pub fn get_integer(&self, key: &Key) -> Option<i64> {
         if let Some(value) = self.get_value(key) {
             match value.value() {
                 Value::Integer(result) => Some(*result),
                 _ => None,
-            }           
+            }
         } else {
             None
         }
     }
 
-    pub fn get_float(&self, key: &SmolStr) -> Option<f64> {
+    pub fn get_float(&self, key: &Key) -> Option<f64> {
         if let Some(value) = self.get_value(key) {
             match value.value() {
                 Value::Float(result) => Some(*result),
                 _ => None,
-            }           
+            }
         } else {
             None
         }
     }
 
-    pub fn get_boolean(&self, key: &SmolStr) -> Option<bool> {
+    pub fn get_boolean(&self, key: &Key) -> Option<bool> {
         if let Some(value) = self.get_value(key) {
             match value.value() {
                 Value::Boolean(result) => Some(*result),
                 _ => None,
-            }           
+            }
         } else {
             None
         }
     }
 
-    pub fn get_color(&self, key: &SmolStr) -> Option<Color> {
+    pub fn get_color(&self, key: &Key) -> Option<Color> {
         if let Some(value) = self.get_value(key) {
             match value.value() {
                 Value::Color(result) => Some(*result),
                 _ => None,
-            }           
+            }
         } else {
             None
         }
     }
 
-    pub fn get_path(&mut self, key: &SmolStr) -> Option<PathBuf> {
+    pub fn get_path(&mut self, key: &Key) -> Option<PathBuf> {
         if let Some(value) = self.get_value(key) {
             match value.value() {
                 Value::Path(result) => Some(result.clone()),
                 _ => None,
-            }           
+            }
         } else {
             None
         }
     }
-    
-    pub fn get_string(&mut self, key: &SmolStr) -> Option<SmolStr> {
+
+    pub fn get_string(&mut self, key: &Key) -> Option<SmolStr> {
         if let Some(value) = self.get_value(key) {
             match value.value() {
                 Value::String(result) => Some(result.clone()),
                 _ => None,
-            }           
+            }
         } else {
             None
         }
--- config/src/lib.rs
+++ config/src/lib.rs
@@ -4,4 +4,5 @@ mod gui;
 pub mod workdir;
 
 pub use app::AppConfig;
+pub use config::Config;
 pub use gui::{GuiConfig, InterfaceMode};
